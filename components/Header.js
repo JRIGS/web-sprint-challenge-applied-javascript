e9b349522c5bce66f0ed3fb71a9afd7f4f9450ab
JRIGS <raczkowskijonathan@gmail.com>
2020-08-07 12:13:54 -0400

    Finished Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,32 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+const selectHeader = document.querySelector('.header-container')
 
-function Header() {}
+function Header() {
+
+const header = document.createElement('div')
+const date = document.createElement('span')
+const title = document.createElement('h1')
+const temperature = document.createElement('span')
+
+header.appendChild(date)
+header.appendChild(title)
+header.appendChild(temperature)
+
+header.classList.add("header")
+date.classList.add("date")
+temperature.classList.add("temperature")
+
+date.textContent = "MARCH 28, 2020"
+title.textContent = "Lambda Times"
+temperature.textContent = "98°"
+
+
+selectHeader.appendChild(header)
+
+return header
+
+}
+
+Header()
